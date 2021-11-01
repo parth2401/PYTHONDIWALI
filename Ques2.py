cdea3f58bcdb184c00284e42317763923c1fc633
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-01 17:50:35 +0530

    Create Ques2.py

---
+++ Ques2.py
@@ -0,0 +1,13 @@
+def multiply_nos(a,b):
+    multiply = a*b 
+    return multiply 
+    
+x = int(input("enter first number"))
+y = int(input("enter first number"))
+result= multiply_nos(x , y)
+print("Multiplication of two numbers:", result)
+
+
+enter first number6
+enter first number4
+Multiplication of two numbers: 24
