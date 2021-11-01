c5d0b97bd801186b6b04f3092448430af878c5d9
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-01 19:02:29 +0530

    Create ques8.py

---
+++ ques8.py
@@ -0,0 +1,12 @@
+def new_func():
+    a = int(input("Enter the first number : "))
+    b = int(input("Enter the second number : "))
+    c = a +b 
+
+
+    if c > 100 :
+        print(a*b)  
+    else : 
+        print(c)
+
+new_func()
