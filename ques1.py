fe8eb38338e0b0eef03f42944bce9a958ddd74fe
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-01 18:23:54 +0530

    Create ques1.py

---
+++ ques1.py
@@ -0,0 +1,14 @@
+x = 3.1415926
+y = 12.9999
+print("\nOriginal Number: ", x)
+print("Formatted Number: "+"{:.2f}".format(x));
+print("Original Number: ", y)
+print("Formatted Number: "+"{:.2f}".format(y));
+print()  
+
+Output:
+    
+Original Number:  3.1415926                                                                                   
+Formatted Number: 3.14                                                                                        
+Original Number:  12.9999                                                                                     
+Formatted Number: 13.00 
