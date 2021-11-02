180c603d0b5682994498844af53e99499436b35a
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-02 21:37:07 +0530

    Create Ques9.py

---
+++ Ques9.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter first number : "))
+c = int(input("Enter first number : ")) 
+
+if ((a != b) or (b != c) or (c != a)) :
+    print(4*(a+b+c))
+else : 
+    print(a+b+c)
