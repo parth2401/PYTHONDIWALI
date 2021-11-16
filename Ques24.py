0b388ec99267ad956b7daa0d8b8847ecec06fe2d
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-16 18:18:31 +0530

    Create Ques24.py

---
+++ Ques24.py
@@ -0,0 +1,26 @@
+num = int(input("Enter a number: "))
+
+
+sum = 0
+
+
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+    Output 1
+
+Enter a number: 663
+663 is not an Armstrong number
+
+Output 2
+
+Enter a number: 407
+407 is an Armstrong number
