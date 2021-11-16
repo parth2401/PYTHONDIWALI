83d32dcee5e3943307ecb632c5cd87edf9fd0aa6
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-11-16 18:21:03 +0530

    Create Ques26.py

---
+++ Ques26.py
@@ -0,0 +1,24 @@
+
+sum1=0
+num=int(input("Enter a number:"))
+temp=num
+while(num):
+    i=1
+    f=1
+    r=num%10
+    while(i<=r):
+        f=f*i
+        i=i+1
+    sum1=sum1+f
+    num=num//10
+if(sum1==temp):
+    print("The number is a strong number")
+else:
+    print("The number is not a strong number")
+    
+    OUTPUT 1:
+Enter a number:145
+The number is a strong number.
+OUTPUT 2:
+Enter a number:234
+The number is not a strong number.
