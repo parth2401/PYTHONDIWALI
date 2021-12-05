631aa95e4f7c338abe79975c1ed08e3c538d506e
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-12-05 11:03:03 +0530

    Create  Ques35.py

---
+++ Ques35.py
@@ -0,0 +1,18 @@
+str1 = input("Please Enter First String : ")
+str2 =input("Please Enter Second String : ")
+ 
+x=str1[0:2]
+ 
+str1=str1.replace(str1[0:2],str2[0:2])
+ 
+str2=str2.replace(str2[0:2],x)
+ 
+print("Your first string has become :- ",str1)
+print("Your second string has become :- ",str2)
+
+# output:
+# Please Enter First String :  sam
+# Please Enter Second String :  mak
+
+# Your first has become :-  mam
+# Your second has become :-  sak
