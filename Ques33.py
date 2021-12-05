451dd19dbb277a8ede57a37082ec73abf8b17f02
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-12-05 11:02:23 +0530

    Create  Ques33.py

---
+++ Ques33.py
@@ -0,0 +1,17 @@
+n = int(input("Enter number of rows: "))
+a = 65 
+for i in range(1,n+1):
+    for j in range(1, i+1):
+        print("%c" %(a), end="")
+    a +=1
+    print()
+    
+    OUTPUT :- 
+    A 
+    BB 
+    CCC 
+    DDDD 
+    EEEEE 
+    FFFFFF 
+    GGGGGGG 
+    HHHHHHHH
