a3641e66d258bf7cc3a0b83849d4e00d039032b5
parth2401 <68024772+parth2401@users.noreply.github.com>
2021-12-05 11:16:10 +0530

    Create Ques21.py

---
+++ Ques21.py
@@ -0,0 +1,21 @@
+rows = 5
+for i in range(rows):
+    for j in range(i):
+        print("*", end=' ')
+    print('')
+num = rows
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("*", end=' ')
+    print("\r") 
+    
+    OUTPUT :- 
+       * 
+       * * 
+       * * * 
+       * * * * 
+       * * * * *
+       * * * *
+       * * *
+       * *
+       *
